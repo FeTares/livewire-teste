ebda310fe0b0116bf58d75662375e72c091387da
Felipe Tavares <felipe.amtavares1@gmail.com>
2021-01-21 11:58:47 -0300

    Upload Foto Usuario

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\ShowTweets;
+use App\Http\Livewire\User\UploadPhoto;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tweets', ShowTweets::class)->middleware('auth');
+Route::get('/tweets', ShowTweets::class)
+            ->name('tweets.index')
+            ->middleware('auth');
+Route::get('/upload', UploadPhoto::class)
+            ->name('upload.photo.user');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
